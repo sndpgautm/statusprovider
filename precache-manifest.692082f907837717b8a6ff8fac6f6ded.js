0530fced1df868b179fbcb17ae97008a7b8e39d2
Sandip Gautam <rockersandip96@gmail.com>
2019-10-29 17:08:26 +0200

    Updates

---
+++ precache-manifest.692082f907837717b8a6ff8fac6f6ded.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c75d05be7add4142da0e1dae0bf54676",
+    "url": "/statusprovider/index.html"
+  },
+  {
+    "revision": "93d8f4ee947e4cf7d738",
+    "url": "/statusprovider/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "7731366f9c4d7cfa35d2",
+    "url": "/statusprovider/static/css/main.66b127e2.chunk.css"
+  },
+  {
+    "revision": "93d8f4ee947e4cf7d738",
+    "url": "/statusprovider/static/js/2.974ae6e6.chunk.js"
+  },
+  {
+    "revision": "7731366f9c4d7cfa35d2",
+    "url": "/statusprovider/static/js/main.3c2a1a92.chunk.js"
+  },
+  {
+    "revision": "c824f7b5813142af0f2b",
+    "url": "/statusprovider/static/js/runtime-main.da1bacaa.js"
+  }
+]);
